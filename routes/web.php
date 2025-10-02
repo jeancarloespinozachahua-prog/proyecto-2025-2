1afc40f7e9de09758264dbb42f1bb55ee793951c
JEAN <jeancarloespinozachahua@gmail.com>
2025-10-02 10:41:52 -0500

    Archivo vista de bienvenido

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludos', function () {
+    return 'hola amiguitos';
+});
+
+Route::get('/saludos', function () {
+    return 'bienvenidos';
+});
